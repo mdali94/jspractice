9031582f574a0078131d2e354ede6b0a8894c8d6
mdali94 <mohammadali9276@gmail.com>
2022-03-19 19:49:44 +0100

    data type

--- index2.js
+++ index2.js
@@ -1,1 +1,18 @@
-var countrys=["bangaldesh","germany","finland",]
+// keyword, data type, comment
+// primitive - number, boolean, string, undefined
+// Object - array, object, null
+let name = "ali muhammed";
+let age = 25;
+let isStudent = true;
+let cgpa = 3.75;
+let user = null;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isStudent)
+console.log(typeof cgpa)
+console.log(typeof user)
+
+
+
+
